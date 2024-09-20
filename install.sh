93b028a257adc00d761284c5e43c80d7e0bc3b27
baefthde <baefthde@gmail.com>
2024-09-20 14:00:39 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo " "
+echo "  Installation von OpenWebUI_De"
+echo "  von https://github.com/baefthde/OpenWebUI_De/"
+echo " "
+echo "  System aktualisieren"
+sudo apt-get update
+sudo apt-get upgrade
+echo "  Abhänigkeiten installieren"
+sudo apt-get install git
+echo "  Paket per Git laden"
+git clone https://github.com/baefthde/OpenWebUI_De
+echo "  Paket starten"
+cd OpenWebUI_De
+./OpenWebUI_De.sh
